16e0b25d192562b4e44275d41c9804595bd88c49
moahmed amine mezghich <git@mezghich.com>
2023-12-08 11:01:38 +0100

    lecture de deux comptes bancaires

--- src/main/java/com/sopra/CompteBancaire.java
+++ src/main/java/com/sopra/CompteBancaire.java
@@ -1,17 +1,29 @@
 package com.sopra;
 
+import java.util.Date;
+
 public class CompteBancaire {
 	
 	private int numCompte;
 	private String proprietaire;
 	private double solde;
 	private String typeCompte;
-	public CompteBancaire(int numCompte, String proprietaire, double solde, String typeCompte) {
+	public Date getDateCreate() {
+		return dateCreate;
+	}
+
+	public void setDateCreate(Date dateCreate) {
+		this.dateCreate = dateCreate;
+	}
+
+	private Date dateCreate;
+	public CompteBancaire(int numCompte, String proprietaire, double solde, String typeCompte, Date dateCreate) {
 		super();
 		this.numCompte = numCompte;
 		this.proprietaire = proprietaire;
 		this.solde = solde;
 		this.typeCompte = typeCompte;
+		this.dateCreate = dateCreate;
 	}
 	
 	public CompteBancaire() {
@@ -52,8 +64,10 @@ public class CompteBancaire {
 	@Override
 	public String toString() {
 		return "CompteBancaire [numCompte=" + numCompte + ", proprietaire=" + proprietaire + ", solde=" + solde
-				+ ", typeCompte=" + typeCompte + "]";
+				+ ", typeCompte=" + typeCompte + ", dateCreate=" + dateCreate + "]";
 	}
+
+	
 	
 
 }
--- src/main/java/com/sopra/SaisieComptes.java
+++ src/main/java/com/sopra/SaisieComptes.java
@@ -1,10 +1,53 @@
 package com.sopra;
 
+import java.util.Date;
+import java.util.Scanner;
+
 public class SaisieComptes {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Our Bank");
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		// faire la saisie de deux comptes
+		
+		
+		 System.out.println(newAccount(scanner,"Courant"));
+		 System.out.println("****");
+		 System.out.println(newAccount(scanner,"Epargne"));
+		//faire la sauvegarde dans un fichier xml
+		
+		
+		//faire le lecture des comptes bancaires
 
+	}
+	
+	public static CompteBancaire newAccount(Scanner scanner, String typeCompte) {
+		
+		CompteBancaire cb = new CompteBancaire();
+		
+		System.out.println("Donner le numéro de compte");
+		int numCompte = scanner.nextInt();
+		cb.setNumCompte(numCompte);
+		
+		System.out.println("Donner le nom du proprietaire du compte");
+		String nom = scanner.next();
+		cb.setProprietaire(nom);
+		
+		System.out.println("Donner le solde initial");
+		double solde = scanner.nextDouble();
+		cb.setSolde(solde);
+		
+		//System.out.println("Donner le type de compte : Epargne ou Courant");
+		//String typeCompte = scanner.next();
+		cb.setTypeCompte(typeCompte);
+		
+		cb.setDateCreate(new Date());
+		
+		return cb;
+		
+		
 	}
 
 }
